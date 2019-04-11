9fb4dfada048a0c46598fe163ccaddbeb140301c
johannag285 <johannag285@gmail.com>
2019-04-10 20:37:50 -0500

    add pages grupos

--- AKcode/src/app/app.module.ts
+++ AKcode/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './Pages/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './Pages/home/home.component';
 import { FooterComponent } from './Shared/footer/footer.component';
+import { GruposComponent } from './Pages/grupos/grupos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    GruposComponent
   ],
   imports: [
     BrowserModule,
