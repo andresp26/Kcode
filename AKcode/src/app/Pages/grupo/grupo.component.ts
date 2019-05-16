9653c9d37f84e6a68e7af92dcee2da86d3f553e4
cafc1234 <camilofique@hotmail.com>
2019-05-15 21:25:12 -0500

    Boton seguimiento
    Ahora se valida si un usuario sigue o no un grupo y en base a eso cambia el botón, en dado caso de que intente seguir a un grupo que ya sigue muestra un error

--- AKcode/src/app/Pages/grupo/grupo.component.ts
+++ AKcode/src/app/Pages/grupo/grupo.component.ts
@@ -4,6 +4,8 @@ import { GrupoService } from '../../service/grupo.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { Grupo } from '../../Models/Grupo.class';
 import { map } from 'rxjs/operators';
+import { AlertService } from 'ngx-alerts';
+
 
 @Component({
   selector: 'app-grupo',
@@ -15,9 +17,10 @@ export class GrupoComponent implements OnInit {
   KeyGrupo = '';
   Grupo: any;
   CantidadSeguidores: number;
+  esSeguidor:boolean;
   
   constructor(private router: Router, public _serviceGrupo: GrupoService,
-    private spinner: NgxSpinnerService, private route: ActivatedRoute) { }
+    private spinner: NgxSpinnerService, private route: ActivatedRoute,private alertas: AlertService) { }
 
   ngOnInit() {
     this.Grupo = new Grupo();
@@ -27,19 +30,41 @@ export class GrupoComponent implements OnInit {
            ).subscribe((data) => {
               this.Grupo = data;
               console.log(this.Grupo);
-              this.ValidaSeguidor();
+              this.esSeguidor=this.ValidaSeguidor(localStorage.getItem('Usuario'),this.Grupo.seguidores);
+              console.log(this.esSeguidor);
               this.CantidadSeguidores=Object.keys(this.Grupo.seguidores).length;
 
           });
     });
   }
+   //Valida si un usuario es seguidor de un grupo o no 
+  ValidaSeguidor(usuario,seguidores) {
+      let keys=Object.keys(seguidores);
+      let existe=false;
+      keys.forEach(function(value){
+          let usuarioTemporal=seguidores[value].username;
+          if(usuarioTemporal==usuario){
+               existe= true;
+          }
+      });
+      return existe;
 
-  ValidaSeguidor() {
-    // this.Grupo.seguidores
   }
 
   AddSeguidor() {
-     this._serviceGrupo.addSeguidor(localStorage.getItem('Usuario'), this.KeyGrupo);
+    if(!this.esSeguidor){
+      this._serviceGrupo.addSeguidor(localStorage.getItem('Usuario'), this.KeyGrupo);
+      this.alertas.success('Has seguido al grupo de forma exitosa');
+
+    }else{
+      this.alertas.danger('Ya sigues al grupo, no puedes seguirlo de nuevo');
+
+    }
+  }
+
+  EliminarSeguidor(){
+    this.alertas.warning('Aún no se programa esta función');
+
   }
 
 
